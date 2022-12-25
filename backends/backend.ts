f926c8320ed3d9ceaa213214c590f88cdc48d4a5
Marcos Candeia <marrcooos@gmail.com>
2022-12-25 09:44:20 -0300

    Support visible at attr
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>

---
+++ backends/backend.ts
@@ -0,0 +1,28 @@
+import { HistoryEvent } from "../events.ts";
+import { PromiseOrValue } from "../promise.ts";
+
+export interface TransactionExecutor {
+  add(events: HistoryEvent[]): void;
+  addPending(events: HistoryEvent[]): void;
+}
+
+/**
+ * Backend is the storage backend used for the workflows.
+ */
+export interface Backend {
+  /**
+   * within transaction executes commands inside a transaction providing the ACID guarantees
+   * if the executor function returns an exception, the transaction should be rolled back, otherwise it should commit all changes atomically.
+   * when executing the given function any operation should be inside a lock machanism avoiding double execution in progress.
+   * @param instanceId the instance Id
+   * @param exec the execution func
+   */
+  withinTransaction<T>(
+    instanceId: string,
+    exec: (
+      events: HistoryEvent[],
+      pendingEvents: HistoryEvent[],
+      transactor: TransactionExecutor
+    ) => PromiseOrValue<T>
+  ): Promise<T>;
+}
--- executor.ts
+++ executor.ts
@@ -1,6 +1,6 @@
-import { inMemoryBackend } from "./backend.ts";
+import { inMemoryBackend } from "./backends/memory/db.ts";
 import { WorkflowContext } from "./context.ts";
-import { apply } from "./events.ts";
+import { apply, HistoryEvent } from "./events.ts";
 import { WorkflowState, zeroState } from "./state.ts";
 import { Arg } from "./types.ts";
 import { Workflow, WorkflowGenFn, WorkflowGen } from "./workflow.ts";
@@ -11,31 +11,43 @@ export function runWorkflow<TArgs extends Arg = Arg, TResult = unknown>(
   instanceId: string,
   workflow: Workflow<TArgs, TResult>
 ): Promise<WorkflowState<TArgs, TResult>> {
-  return backend.withinTransaction(instanceId, async (events, { add }) => {
-    const ctx = new WorkflowContext(instanceId);
-    const workflowFn: WorkflowGenFn<TArgs, TResult> = (
-      ...args: [...TArgs]
-    ): WorkflowGen<TResult> => {
-      return workflow(ctx, ...args);
-    };
+  return backend.withinTransaction(
+    instanceId,
+    async (events, pendingEvents: HistoryEvent[], { addPending, add }) => {
+      const ctx = new WorkflowContext(instanceId);
+      const workflowFn: WorkflowGenFn<TArgs, TResult> = (
+        ...args: [...TArgs]
+      ): WorkflowGen<TResult> => {
+        return workflow(ctx, ...args);
+      };
 
-    let state: WorkflowState<TArgs, TResult> = events.reduce(
-      apply,
-      zeroState(workflowFn)
-    );
+      let state: WorkflowState<TArgs, TResult> = [
+        ...events,
+        ...pendingEvents,
+      ].reduce(apply, zeroState(workflowFn));
 
-    // this should be done by a command handler executor in background.
-    while (
-      !(
-        state.hasFinished ||
-        state.cancelledAt !== undefined ||
-        state.current.isCompleted
-      )
-    ) {
-      const newEvents = await state.current.run(backend);
-      state = newEvents.reduce(apply, state);
-      add(newEvents);
+      add(pendingEvents);
+
+      // this should be done by a command handler executor in background.
+      while (
+        !(
+          state.hasFinished ||
+          state.cancelledAt !== undefined ||
+          state.current.isCompleted
+        )
+      ) {
+        const newEvents = await state.current.run();
+        state.current.changeState("Completed");
+        newEvents.forEach((event) => {
+          if (event.visibleAt) {
+            addPending([event]);
+          } else {
+            state = apply(state, event);
+            add([event]);
+          }
+        });
+      }
+      return state;
     }
-    return state;
-  });
+  );
 }
